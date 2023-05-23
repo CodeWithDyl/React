26f4da0c39f6b17477fa05ff8c988f6be0125ce9
Dylan J Dombrowski <d0domb01@louisville.edu>
2023-05-23 11:45:00 -0400

    finished Mosh's tutorial!

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,21 +1,18 @@
-// import ListGroup from "./components/ListGroup";
-
-import Alert from "./components/alert";
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
-  // let items = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
+  const [alertVisible, setAlertVisibility] = useState(false);
 
-  // const handleSelectItem = (item: string) => {
-  //   console.log(item);
-  // };
   return (
     <div>
-      <Alert />
-      {/* <ListGroup
-        items={items}
-        heading={"Cities"}
-        onSelectItem={handleSelectItem}
-      /> */}
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
+      )}
+      <Button color="danger" onClick={() => setAlertVisibility(true)}>
+        My Button
+      </Button>
     </div>
   );
 }
--- react-app/src/components/Alert.tsx
+++ react-app/src/components/Alert.tsx
@@ -1,5 +1,23 @@
-const Alert = () => {
-  return <div>alert</div>;
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+  onClose: () => void;
+}
+
+const Alert = ({ children, onClose }: Props) => {
+  return (
+    <div className="alert alert-success alert-dismissable">
+      {children}
+      <button
+        type="button"
+        className="btn-close"
+        onClick={onClose}
+        data-bs-dismiss="alert"
+        aria-label="Close"
+      ></button>
+    </div>
+  );
 };
 
 export default Alert;
